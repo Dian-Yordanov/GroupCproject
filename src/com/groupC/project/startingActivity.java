746429ccf22fc5c19501fc8b5b708559aa9b6020
Xynnect <dido.jordanov7@gmail.com>
2013-10-29 16:32:54 +0000

    Working menu that takes you to Indicators

--- src/com/groupC/project/startingActivity.java
+++ src/com/groupC/project/startingActivity.java
@@ -1,19 +1,44 @@
 package com.groupC.project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+
 
 public class startingActivity  extends Activity {
+	
+	Button countryList;
+	Button indicators;
+	Button favourites;
+	Button aboutUs;
+	
     
 @Override
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.starting_activity);   
     
-    Log.v("sdsd","sdsd");
+    indicators = (Button)findViewById(R.id.Indicators);
     
     
+}
+
+/*
+public void gotoCountryList(View view){
+    Intent i = new Intent(startingActivity.this, MainActivity.class);
+    startActivity(i);    
+
+}
+*/
+
+public void gotoIndicators(View view){
+    Intent i = new Intent(startingActivity.this, MainActivity.class);
+    startActivity(i);    
+
 }
 
 }
