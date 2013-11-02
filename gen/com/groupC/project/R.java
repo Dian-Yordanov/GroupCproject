82e77e5715da1bdffc073f9f345b83e7653aacad
Xynnect <dido.jordanov7@gmail.com>
2013-11-02 17:49:29 +0000

    added countryArrayWithOnly2letters string-array

--- gen/com/groupC/project/R.java
+++ gen/com/groupC/project/R.java
@@ -9,8 +9,10 @@ package com.groupC.project;
 
 public final class R {
     public static final class array {
+        public static final int countryArrayWithOnly2letters=0x7f060001;
         public static final int countryListView=0x7f060000;
-        public static final int indicatorListView=0x7f060001;
+        public static final int countryNames=0x7f060002;
+        public static final int indicatorListView=0x7f060003;
     }
     public static final class attr {
     }
