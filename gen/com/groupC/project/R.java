39244a76f7dd44dcde3914ea62ba2e493f367851
Xynnect <dido.jordanov7@gmail.com>
2013-11-02 18:45:00 +0000

    Added full country names into a country array

--- gen/com/groupC/project/R.java
+++ gen/com/groupC/project/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class array {
         public static final int countryArrayWithOnly2letters=0x7f060001;
         public static final int countryListView=0x7f060000;
-        public static final int indicatorListView=0x7f060002;
+        public static final int countryNames=0x7f060002;
+        public static final int indicatorListView=0x7f060003;
     }
     public static final class attr {
     }
