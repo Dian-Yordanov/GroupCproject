1f5daf48a2c9b14fd75a490d423a21ca9734105d
odaynasser <oday@outlook.com>
2013-11-12 17:26:11 +0000

    some changes

--- src/com/groupC/project/ImageDownloader.java
+++ src/com/groupC/project/ImageDownloader.java
@@ -16,25 +16,6 @@ import android.widget.ImageView;
 public class ImageDownloader {
 	public ImageDownloader(){}
 	
-	/*
-	 * http://chart.googleapis.com/chart?chs=420x220&cht=map:auto&chco=B3BCC0|5781
-	 * AE&chld=CN&chdl=China%20mainland =China%20mainland are the labels that we
-	 * don't need
-	 * http://chart.googleapis.com/chart?chs=420x220&cht=map:auto&chco
-	 * =B3BCC0|5781AE&chld=CN&chdl &chdl is the label starter in general:
-	 * &chdl=China%20mainland LABEL
-	 * http://chart.googleapis.com/chart?chs=420x220
-	 * &cht=map:auto&chco=B3BCC0|5781AE&chld=CN CN is the name of the country
-	 * http
-	 * ://chart.googleapis.com/chart?chs=420x220&cht=map:auto&chco=B3BCC0|5781
-	 * AE&chld=CN 5781 is the color
-	 * http://chart.googleapis.com/chart?chs=420x220
-	 * &cht=map:auto&chco=B3BCC0|5781AE&chld=CN 420x220 sizes but they don't
-	 * work in some combinations so we may need to see if it is going to work
-	 * for the popular models and what are like the resolution requirements any
-	 * way there are like 3 things that the query builder can change : 420x220
-	 * (resolution) CN name of country
-	 */
 	
 	public static Bitmap loadBitmap(String url){
 		Bitmap bitmap = null;
--- src/com/groupC/project/QueryBuilder.java
+++ src/com/groupC/project/QueryBuilder.java
@@ -1,48 +1,6 @@
 package com.groupC.project;
-/**
-		jsonParserReader is passing the Json data from JsonParser.java
-		also in oirder to make the parser working i need a sdk permission: 
-		<uses-sdk android:minSdkVersion="9" android:targetSdkVersion="17" />
-		<uses-permission
-		android:name="android.permission.INTERNET"></uses-permission>
-		i am also using a textView1 to display the source Json
-		the think is that son.readData(String) reads only about that string
-		and that string should be changed
-		 
-		countryAndIndicatorQueryConstructor() will construct api calls
-		http://api.worldbank.org/countries/ABW/indicators/1.1_TOTAL.FINAL.ENERGY.CONSUM?per_page=50&date=1960:2013&format=json
-		http://api.worldbank.org/countries/BGR/indicators/1.1_ACCESS.ELECTRICITY.TOT?per_page=10&date=1960:2013&format=json
-		the only 2 changing parts of the api calling string are the ones
-		between http://api.worldbank.org/countries/ & /indicators/ and
-		/indicators/ & per_page=50&date=1960:2013&format=json
-	 	in other words you can just remove
-		http://api.worldbank.org/countries/ ; /indicators/ ;
-		per_page=50&date=1960:2013&format=json
-		Jcat will get the string and will replace the 2 important parts with
-		its own strings who will be arrays
-		http://api.worldbank.org/countries/{country_id}/indicators/{indicator_id}?date={from_year}:{to_year}
-		this is the full string type bujt we will leave {fromyear},{toyear}
-		to 1960 and 2013
-		user changes different countryName and indicatorName from a list and
-		get's the query for them
+
 		
-		countryQueryConstructor() will construct api calls 
-	 	while the countryAndIndicatorQueryConstructor() is constructing queries by country name and by the indicator this query constructor will make it only with country calls
-	 	http://api.worldbank.org/countries/AFG?per_page=50&date=1960:2013&format=json
-	 	http://api.worldbank.org/countries/ 	part 1
-	 	AFG
-	 	all									part 2
-     	AB
-      	/indicators/						part 3
-     	SP.POP.TOTL  						part 4
-     	? 									part 5 
-	 	per_page=25  						part 6 
-     	&            						part 7
-     	format=json  						part 8 
-	 	now the query is build from part1 + part2 + part 5 + part 6 + part 7 + part 8 and in order to just use the parts we all ready have from countryAndIndicatorQueryConstructor() 
-	 	we will use part1, part 2 and part 5 + part 6 + part 7 + part 8
-	 	where part 1 = QueryBuilder.part1 ; part 2 = countryName; part 5 to part 8 = QueryBuilder.part5
- */
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -139,9 +97,12 @@ public class QueryBuilder {
 			for (int i = 0; i < countries.length(); i++) {
 				jsonInfo = (JSONObject) countries.get(i);
 
-				if(nameOftheClassCallingThisClass=="IndicatorActivity")jsonObjectExtractorForCountryAndIndicator();
-				if(nameOftheClassCallingThisClass=="CountryActivity")jsonObjectExtractorForCountry();
-				if(nameOftheClassCallingThisClass=="ComparisonActivity")jsonObjectExtractorForCountryAndIndicator();
+				if(nameOftheClassCallingThisClass=="IndicatorActivity")
+					jsonObjectExtractorForCountryAndIndicator();
+				if(nameOftheClassCallingThisClass=="CountryActivity")
+					jsonObjectExtractorForCountry();
+				if(nameOftheClassCallingThisClass=="ComparisonActivity")
+					jsonObjectExtractorForCountryAndIndicator();
 				
 			}
 		} catch (JSONException e) {
