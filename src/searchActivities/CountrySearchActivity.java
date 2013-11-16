26679a4a11157bc9d5b0da475de406006dc4892b
Xynnect <dido.jordanov7@gmail.com>
2013-11-16 19:51:26 +0000

    made the EditView unfocused on start

--- src/searchActivities/CountrySearchActivity.java
+++ src/searchActivities/CountrySearchActivity.java
@@ -6,6 +6,7 @@ import com.groupC.project.R.layout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -20,9 +21,25 @@ public class CountrySearchActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		comparisonSearchActivityBuildUi();
+		
+	}
+	private void comparisonSearchActivityBuildUi(){
 		setContentView(R.layout.country_search_activity);
 		countryText = (TextView) findViewById(R.id.countryText);
+		selectYourCountryEditText = (EditText) findViewById(R.id.selectYourCountryEditText);
+		countriesListView = (ListView) findViewById(R.id.countriesListView);
+		
+		
+		selectYourCountryEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+		    @Override
+		    public void onFocusChange(View v, boolean hasFocus) {
+		        if (hasFocus) {
+		        	selectYourCountryEditText.setText("");
+		        }
+		    }
+			});
 	}
-	private void comparisonSearchActivityBuildUi(){}
+	
 		
 }
