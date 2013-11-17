618a7b6f673226492519d5ec0b9d343ceca3ea10
Sekban <semihersahin@gmail.com>
2013-11-17 16:48:41 +0000

    Update CountryPicturesQueryBuilder.java
    Added the second flag call for the 15 countries that do not respond to the first call, still need to work on it as the app is not displaying them yet

--- src/com/groupC/project/CountryPicturesQueryBuilder.java
+++ src/com/groupC/project/CountryPicturesQueryBuilder.java
@@ -7,6 +7,11 @@ public class CountryPicturesQueryBuilder {
 	private static String urlBuilderFlagsBeginning = "http://flagpedia.net/data/flags/normal/";
 	private static String urlBuilderFlagsEnd =".png";
 	public static String countryCode= "";
+	
+//taking country flags from http://www.flagpedia.com/SiteImages/Data/ and using the string from
+	private static String urlBuilderFlagsBeginning2 = "http://www.flagpedia.com/SiteImages/Data/1";
+	private static String urlBuilderFlagsEnd2 =".jpg";
+	public static String countryCode2= "";
 
 //taking country maps from http://i.infoplease.com/images/mlebanon.gif
 	private static String urlBuilderCountryLocationBeginning = "http://i.infoplease.com/images/m";
