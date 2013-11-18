aba6d2253fd2233a976796a2f3ad8948a47ec327
Xynnect <dido.jordanov7@gmail.com>
2013-11-18 14:32:24 +0000

    SearchActivityies are now working

--- src/displayActivities/ComparisonActivity.java
+++ src/displayActivities/ComparisonActivity.java
@@ -42,8 +42,8 @@ public class ComparisonActivity extends Activity{
 		textViewComparison = (TextView) findViewById(R.id.textView1);
 		textViewComparison.setText(ComparisonSearchActivity.textViewComparisonText1 + ComparisonSearchActivity.textViewComparisonText2);
 		
-        //graphViewLayout = (LinearLayout) findViewById(R.id. layout2); 
-       // GraphViewCreator. graphViewCreator();
+        graphViewLayout = (LinearLayout) findViewById(R.id. layout2); 
+        GraphViewCreator. graphViewCreator();
 
 	}
 }
