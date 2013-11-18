92342a859765e9661b67cf949914a48d055a6ce9
Xynnect <dido.jordanov7@gmail.com>
2013-11-18 14:48:36 +0000

    merging Update CountryPicturesQueryBuilder.java

--- src/logicClasses/CountryPicturesQueryBuilder.java
+++ src/logicClasses/CountryPicturesQueryBuilder.java
@@ -9,16 +9,23 @@ import android.graphics.Bitmap;
 import android.util.Log;
 
 public class CountryPicturesQueryBuilder {
-//taking country logos from http://flagpedia.net/data/flags/normal/fr.png and using the string from 
+	// taking country logos from http://flagpedia.net/data/flags/normal/fr.png
+	// and using the string from
 	private static String urlBuilderFlagsBeginning = "http://flagpedia.net/data/flags/normal/";
-	private static String urlBuilderFlagsEnd =".png";
-	public static String countryCode= "";
+	private static String urlBuilderFlagsEnd = ".png";
+	public static String countryCode = "";
 
-//taking country maps from http://i.infoplease.com/images/mlebanon.gif
+	// taking country flags from http://www.flagpedia.com/SiteImages/Data/ and
+	// using the string from
+	private static String urlBuilderFlagsBeginning2 = "http://www.flagpedia.com/SiteImages/Data/1";
+	private static String urlBuilderFlagsEnd2 = ".jpg";
+	public static String countryCode2 = "";
+
+	// taking country maps from http://i.infoplease.com/images/mlebanon.gif
 	private static String urlBuilderCountryLocationBeginning = "http://i.infoplease.com/images/m";
-	private static String urlBuilderCountryLocationEnd =".gif";
-	public static String countryName= "";
-	
+	private static String urlBuilderCountryLocationEnd = ".gif";
+	public static String countryName = "";
+
 	public static Bitmap flag;
 	public static Bitmap map;
 	
