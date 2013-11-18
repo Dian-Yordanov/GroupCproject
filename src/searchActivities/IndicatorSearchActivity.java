536cf7b0a0b5304e861fd80c4ecf90890f8f9398
Xynnect <dido.jordanov7@gmail.com>
2013-11-18 01:31:56 +0000

    Updates on indicatorSearchActivity

--- src/searchActivities/IndicatorSearchActivity.java
+++ src/searchActivities/IndicatorSearchActivity.java
@@ -1,23 +1,75 @@
 package searchActivities;
 
 import com.groupC.project.*;
+
 import displayActivities.*;
 import logicClasses.*;
 import searchActivities.*;
 
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.widget.*;
 
 public class IndicatorSearchActivity extends Activity{
+	TextView indicatorText;
+	EditText indicatorCountryEditText1;
+	EditText indicatorCountryEditText2;
+	ListView indicatorListView1;
+	ListView indicatorListView2;
+	
+	public static ArrayAdapter<CharSequence> countryAdapter;
+	public static ArrayAdapter<CharSequence> indicatorAdapter;
+	
+	private Resources res;
+	private String[] countries;
+	private String stringUsedForCallingQueryBuilder ="";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.indicator_search_activity);
+		
+		indicatorSearchActivityUiBuilder();
+	//	QueryBuilder.setNameOfClassCallingQueryBuilder(this.getLocalClassName());
+		//QueryBuilder qBuilder = new QueryBuilder(countryAndIndicatorQueryConstructor());
+		
+	//	GraphViewCreator.setNameOfClassCallingGraphViewCreator(this.getLocalClassName());
+		
+		res = getResources();
+		countries= res.getStringArray(R.array.countryListView);
 
 	}
-
+	private void indicatorSearchActivityUiBuilder(){
+	setContentView(R.layout.indicator_search_activity);
+	indicatorText = (TextView) findViewById(R.id.indicatorText);
+	
+	indicatorCountryEditText1 = (EditText) findViewById(R.id.indicatorCountryEditText1);
+	indicatorCountryEditText2 = (EditText) findViewById(R.id.indicatorCountryEditText2);
+	
+	indicatorListView1 = (ListView) findViewById(R.id.indicatorListView1);
+	indicatorListView2 = (ListView) findViewById(R.id.indicatorListView2);
+	
+	createEditOptions(indicatorCountryEditText1);
+	createEditOptions(indicatorCountryEditText2);
+	
+	countryAdapter = ArrayAdapter.createFromResource(this,R.array.countryNames, android.R.layout.simple_list_item_1);
+	indicatorListView1.setAdapter(countryAdapter);
+	
+	indicatorAdapter = ArrayAdapter.createFromResource(this,R.array.indicatorListView, android.R.layout.simple_list_item_1);
+	indicatorListView2.setAdapter(indicatorAdapter);
+	}
+	
+	private void createEditOptions(final EditText editTextToGetOptions) {
+		editTextToGetOptions
+				.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+					@Override
+					public void onFocusChange(View v, boolean hasFocus) {
+						if (hasFocus) {
+							editTextToGetOptions.setText("");
+						}
+					}
+				});
+	}
 }
