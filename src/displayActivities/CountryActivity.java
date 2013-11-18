129850365d558a29da8f3e754e1b0f6992b07584
Xynnect <dido.jordanov7@gmail.com>
2013-11-18 22:32:00 +0000

    Done picture resizing for countryActivity

--- src/displayActivities/CountryActivity.java
+++ src/displayActivities/CountryActivity.java
@@ -13,6 +13,7 @@ import com.groupC.project.R.layout;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -20,6 +21,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -31,6 +34,9 @@ public class CountryActivity  extends Activity{
 	public static ImageView flagView;
 	public static ImageView countryView;
 	public static ArrayAdapter<CharSequence> countryListAdapter;
+	
+	private static Bitmap resizedBitmapFlag;
+	private static Bitmap resizedBitmapMap;
 	
  
 	@Override
@@ -49,14 +55,37 @@ public class CountryActivity  extends Activity{
 		flagView = (ImageView) findViewById(R.id.imageView1);
 		countryView = (ImageView) findViewById(R.id.imageView2);
 		
+
+		imageResize();
+		
 		displayedText.setText(QueryBuilder.displayInfo);
-		flagView.setImageBitmap(CountryPicturesQueryBuilder.flag);
-		countryView.setImageBitmap(CountryPicturesQueryBuilder.map);
+		flagView.setImageBitmap(resizedBitmapFlag);
+		countryView.setImageBitmap(resizedBitmapMap);
+		
 	}
 	public void gotoCountrySearchView(View view) {
 		Intent i = new Intent(CountryActivity.this, CountrySearchActivity.class);
 		startActivity(i);
 
 	}
+	private static void imageResize(){
+		if(CountryPicturesQueryBuilder.map.getWidth() > CountryPicturesQueryBuilder.flag.getWidth()){
+		resizedBitmapFlag =	Bitmap.createScaledBitmap(CountryPicturesQueryBuilder.flag, CountryPicturesQueryBuilder.map.getWidth()
+				, CountryPicturesQueryBuilder.flag.getHeight(), true); 
+		resizedBitmapMap =	Bitmap.createScaledBitmap(CountryPicturesQueryBuilder.map, CountryPicturesQueryBuilder.map.getWidth()
+				, CountryPicturesQueryBuilder.map.getHeight(), true);
+		return;
+		}
+		if(CountryPicturesQueryBuilder.map.getWidth() < CountryPicturesQueryBuilder.flag.getWidth()){
+		resizedBitmapFlag =	Bitmap.createScaledBitmap(CountryPicturesQueryBuilder.flag, CountryPicturesQueryBuilder.flag.getWidth()
+				, CountryPicturesQueryBuilder.flag.getHeight(), true); 
+		resizedBitmapMap =	Bitmap.createScaledBitmap(CountryPicturesQueryBuilder.map, CountryPicturesQueryBuilder.flag.getWidth()
+				, CountryPicturesQueryBuilder.map.getHeight(), true);
+		return;
+		}
+		
+		
+	}
+	
 	
 }
