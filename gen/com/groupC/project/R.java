8dfb4d3fce3d467ac88bc2045eab9a0d0b079437
Xynnect <dido.jordanov7@gmail.com>
2013-11-19 05:11:34 +0000

    some changes to setting bckgrounds of buttons

--- gen/com/groupC/project/R.java
+++ gen/com/groupC/project/R.java
@@ -28,7 +28,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int aboutus=0x7f020000;
+        public static final int ahmet=0x7f020001;
+        public static final int backgruond=0x7f020002;
+        public static final int compare=0x7f020003;
+        public static final int countrylist=0x7f020004;
+        public static final int dian=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int indicators=0x7f020007;
+        public static final int julian=0x7f020008;
+        public static final int oday=0x7f020009;
+        public static final int title=0x7f02000a;
     }
     public static final class id {
         public static final int ComparisonText=0x7f080007;
