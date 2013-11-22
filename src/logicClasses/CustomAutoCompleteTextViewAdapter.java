c2dd092e02709265b8a9cec02a0b2887103a5a65
Xynnect <dido.jordanov7@gmail.com>
2013-11-22 02:07:20 +0000

    some updates on the custom adapter

--- src/logicClasses/CustomAutoCompleteTextViewAdapter.java
+++ src/logicClasses/CustomAutoCompleteTextViewAdapter.java
@@ -32,18 +32,27 @@ public  View view;
     	Log.v("",this.getItem(0));
     	Log.v("",this.getItem(1));
     	Log.v("",Integer.toString(this.getViewTypeCount()));
+    	Log.v("",Integer.toString(this.getCount()));
     	
+    
    
     	this.setDropDownViewResource(R.array.indicatorMeaningListView);
     	
     	final LayoutInflater inflater = LayoutInflater.from(getContext());
         final View view = inflater.inflate(R.layout.dropdown_multiline_item, someViewGroup, false);
+        
+        for(int i=0;i<this.getCount();i++){
+    		Log.v("",this.getItem(i));	
+    		setTextOfTheView(view, i);
+    	}
+        
 		return view;
+		
     }
     
     public void setTextOfTheView(View view, int i){
     	
-    	 ((TextView) view.findViewById(R.id.item)).setText(IndicatorSearchActivity.indicatorNames[i]);
+    	 ((TextView) view.findViewById(R.id.item)).setText(this.getItem(i));
  
     	
     }
