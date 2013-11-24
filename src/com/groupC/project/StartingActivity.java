86a9805836190e78e29987a80f160eb73bf6450c
Xynnect <dido.jordanov7@gmail.com>
2013-11-24 15:43:43 +0000

    no internetError Implemented

--- src/com/groupC/project/StartingActivity.java
+++ src/com/groupC/project/StartingActivity.java
@@ -15,29 +15,57 @@ import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class StartingActivity extends Activity {
-	
+
 	Button countriesSearch;
 	Button indicatorsSearch;
 	Button comparisonSearch;
 	Button aboutUs;
-	
+	private Thread noInternetThread;
+
 	public static int screenWidth;
 	public static int screenHeight;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.starting_activity);
 
-
 		indicatorsSearch = (Button) findViewById(R.id.indicatorsSearch);
 		countriesSearch = (Button) findViewById(R.id.countryListSearch);
 		comparisonSearch = (Button) findViewById(R.id.comparisonSearch);
 		aboutUs = (Button) findViewById(R.id.about);
-		
+
 		gettingTheScreenSize();
+
+	/*	noInternetThread =  new Thread(){
+	        @Override
+	        public void run(){
+	            try {
+	                synchronized(this){
+	                    wait(3000);
+	                }
+	            }
+	            catch(InterruptedException ex){                    
+	            }
+
+	            // TODO              
+	        }
+	    };
+	  
+
+	    noInternetThread.start();  
+	    
+	      */
+	    
+		if (noInternetChecker.isInternetAvailable(StartingActivity.this)){
+			Log.v("there is internet", "how nice");
+		} else {
+			Intent i = new Intent(StartingActivity.this, noInternetError.class);
+			startActivity(i);
+		}
 	}
 
 	public void gotoCountrySearchView(View view) {
