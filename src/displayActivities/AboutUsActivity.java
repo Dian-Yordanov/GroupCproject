9a5b4fe95f593c3834d6dcb5b3347b6336f56feb
Xynnect <dido.jordanov7@gmail.com>
2013-11-24 17:34:26 +0000

    improved aboutUsActivity with the new picture but it is blurry

--- src/displayActivities/AboutUsActivity.java
+++ src/displayActivities/AboutUsActivity.java
@@ -14,26 +14,11 @@ import android.widget.TableLayout.LayoutParams;
 
 
 public class AboutUsActivity extends Activity{
-	LinearLayout odaysLayout;
-	LinearLayout ahmetsLayout;
-	LinearLayout juliansLayout;
-	LinearLayout diansLayout;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.aboutus_activity);
-		
-		/*
-		odaysLayout = (LinearLayout) findViewById(R.id.odaysLayout);
-		ahmetsLayout = (LinearLayout) findViewById(R.id.ahmetsLayout);
-		juliansLayout = (LinearLayout) findViewById(R.id.juliansLayout);
-		diansLayout = (LinearLayout) findViewById(R.id.diansLayout);
-		
-		LayoutParams layoutParamsOday=new LayoutParams(StartingActivity.screenWidth,StartingActivity.screenHeight/2);
-		odaysLayout.setLayoutParams(layoutParamsOday);
-		LayoutParams layoutParamsAhmet=new LayoutParams(0,0);
-		ahmetsLayout.setLayoutParams(layoutParamsAhmet);
-		*/
+
 	}
 
 }
