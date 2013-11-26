defa6179c17a26865b6f274f34ef3af2e5477fb0
Sekban <semihersahin@gmail.com>
2013-11-26 17:13:49 +0000

    Update AboutUsActivity.java

--- src/displayActivities/AboutUsActivity.java
+++ src/displayActivities/AboutUsActivity.java
@@ -7,6 +7,7 @@ import com.groupC.project.R;
 import com.groupC.project.R.layout;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -17,8 +18,30 @@ public class AboutUsActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.aboutus_activity);
-
+		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+			setContentView(R.layout.aboutus_activity);
+		}else{ 
+			setContentView(R.layout.landscape_aboutus_activity);
+		}
+	}
+	private void aboutusActivityBuildUi(boolean IsPortrait){
+		
+		if(IsPortrait==true)
+		{
+			setContentView(R.layout.aboutus_activity);
+		}else{
+			setContentView(R.layout.landscape_aboutus_activity);
+		}
+	}
+	public void onConfigurationChanged (Configuration newConfig){
+		super.onConfigurationChanged(newConfig);
+		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){		
+			aboutusActivityBuildUi(false);
+		}else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+			aboutusActivityBuildUi(true);
+			
+		}
+		
 	}
 
 }
