2bc108f227bd545bac8620a98c8da442d5e28622
Xynnect <dido.jordanov7@gmail.com>
2013-11-28 03:55:25 +0000

    improved the design up to the point of removing black spaces

--- src/displayActivities/ComparisonActivity.java
+++ src/displayActivities/ComparisonActivity.java
@@ -119,16 +119,17 @@ public class ComparisonActivity extends Activity {
 		        
 		        label3 = (TextView)lineView.findViewById(R.id.inflatedTextView3);
 		        label3.setMinimumWidth((StartingActivity.screenWidth/2)-(StartingActivity.screenWidth/6) -32);
-		        label3.setTypeface(null,Typeface.BOLD);
 		        label3.setText(QueryBuilder.arrayWithValuesAndYearsForComparison.get(i));
 		        
 		        if(i%2==0){
 		        	label1.setBackgroundColor(Color.parseColor("#F6F6F6"));
 		        	label2.setBackgroundColor(Color.parseColor("#F6F6F6"));
+		        	label3.setBackgroundColor(Color.parseColor("#F6F6F6"));
 		        }
 		        if(i%4==0){
 		        	label1.setBackgroundColor(Color.parseColor("#CCCCCC"));
 		        	label2.setBackgroundColor(Color.parseColor("#CCCCCC"));
+		        	label3.setBackgroundColor(Color.parseColor("#CCCCCC"));
 		        }
 		        //arrayWithValuesForCountry
 		        }
